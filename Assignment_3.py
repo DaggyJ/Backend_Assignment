805456e9d6ef4dbb80293d7516471c6c90dc6282
Jonson Daggy <156325729+DaggyJ@users.noreply.github.com>
2024-07-09 21:06:32 +0300

    Update Assignment_3.py
    update

--- Assignment_3.py
+++ Assignment_3.py
@@ -5,7 +5,7 @@ def Math_operations(x, y):
 
     '''
     math_operations function takes two integers parameters
-    use of user input is initiated
+    user input is taken as x and y
     uses lambda fuction to calculate sum, product, quotient and difference of the numbers
     lambda arguments : expression
     lambda functions can take any number of arguments
@@ -35,42 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-        #MATHS_OPARETOR THAT TAKES TWO NUMS
-#Definiton of numbers
-def Math_operations(num1, num2):
-    '''
-    math_operations function takes two integers parameters
-    uses lambda fuction to calculate sum, product, quotient and difference of the numbers
-    '''
-    """"
-    lambda arguments : expression
-    lambda functions can take any number of arguments
-    """
-#Lambda functions 
-    mySum = lambda num1, num2 : num1 + num2
-
-    myProduct = lambda num1, num2 : num1 * num2
-
-    myQuotient = lambda num1, num2 : num1 / num2
-
-    myDifference = lambda num1, num2 : num1 - num2
-    #Return function
-    return{
-    "sum" : mySum(num1, num2),
-    "product" : myProduct(num1, num2),
-    "quotient" : myQuotient(num1, num2),
-    "difference" : myDifference(num1, num2)
-}
-
-def main():
-    result = Math_operations(48, 12)
-    print("Sum: ", result["sum"])
-    print("Product: ", result["product"])
-    print("Quotient: ", result["quotient"])
-    print("Difference: ", result["difference"])
-
-if __name__ == '__main__':
-    main()
-
+    
