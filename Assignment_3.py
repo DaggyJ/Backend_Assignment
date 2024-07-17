e8e81ce184f909c4d26bb4b29666c227ac010515
Jonson Daggy <156325729+DaggyJ@users.noreply.github.com>
2024-07-17 14:42:03 +0300

    Update Assignment_3.py
    one insertion made

--- Assignment_3.py
+++ Assignment_3.py
@@ -5,18 +5,13 @@ def Math_operations(x, y):
     Lambda fuction calculates sum, product, quotient and difference of 'x' & 'y'. Lambda functions can take any number of arguments
     lambda arguments : expression
     Return function is use to return the results of sum, product, quotient and difference of 'x' & 'y'
-    main function prints results of Math_operations
+    main function prints results of math_oparetions
    '''
-    x = int(input(f"Enter the value of x: "))
-
-    y = float(input(f"Enter the value of y: "))
-
+   # x = int(input(f"Enter the value of x: "))
+   # y = int(input(f"Enter the value of y: "))
     mySum = lambda x, y : x + y
-
     myProduct = lambda  x, y : x * y
-
     myQuotient = lambda  x, y : x / y
-
     myDifference = lambda  x, y : x - y
 
     return{
@@ -27,7 +22,7 @@ def Math_operations(x, y):
 }
 
 def main():
-    result = Math_operations('x', 'y')
+    result = Math_operations(x = int(input(f"Enter the value of x: ")), y = int(input(f"Enter the value of y: ")))
     print("Sum: ", result["sum"])
     print("Product: ", result["product"])
     print("Quotient: ", result["quotient"])
