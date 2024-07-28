fcd261603f047d94bc91ce086f704f17ed867576
Jonson Daggy <156325729+DaggyJ@users.noreply.github.com>
2024-07-28 17:59:12 +0300

    Update Assignment_5.py
    updated

--- Assignment_5.py
+++ Assignment_5.py
@@ -1,22 +1,25 @@
 import os
 import csv
 
-filePath = 'Cereals_Store.csv'
+filePath = 'products.csv'
 
 def createCSV():
     if not os.path.exists(filePath):
-        with open(filePath, 'w', newline='') as file:
+        with open(filePath, 'w',newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['name', 'quantity'])
+            writer.writerow(header)
+            header = (['name', 'quantity', 'price'])
             print("***********************************\n")
     else:
         print(f"Found existing '{filePath}'.")
 
-def addProducts(name, quantity):
+def addProducts(name, quantity, price):
     try:
         with open(filePath, 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([name, quantity])
+            header = ([name, quantity, price])
+            writer.writerow(header)
+
         print(f"Added '{name}' with quantity '{quantity}'.")
     except csv.Error as e:
         print(f"Error writing '{filePath}': {e}")
@@ -27,13 +30,13 @@ def readProducts():
             reader = csv.DictReader(file)
             print("\n========== Current Products ==========\n")
             for row in reader:
-                print(f"{row['name']:20} {row['quantity']}")
+                print(f"{row['name']:20} {row['quantity']} {row['price']}")
     except FileNotFoundError:
         print(f"Error: '{filePath}' not found")
     except csv.Error as e:
         print(f"Error reading '{filePath}': {e}")
 
-def updateProduct(name, newQuantity):
+def updateProduct(name, newQuantity, price):
     try:
         rows = []
         with open(filePath, 'r', newline='') as file:
@@ -41,16 +44,18 @@ def updateProduct(name, newQuantity):
             for row in reader:
                 if row['name'] == name:
                     row['quantity'] = newQuantity
+                    row['price'] = price
                 rows.append(row)
         with open(filePath, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['name', 'quantity'])
+            writer = csv.DictWriter(file, fieldnames=['name', 'quantity', 'price'])
             writer.writeheader()
             writer.writerows(rows)
-        print(f"Updated quantity for '{name}' to '{newQuantity}'.")
+        print(f"Updated quantity for '{name}' to '{newQuantity}' & '{price}'.")
     except FileNotFoundError:
         print(f"Error: '{filePath}' not found.")
     except csv.Error as e:
         print(f"Error writing '{filePath}': {e}")
+    
 
 def deleteProduct(name):
     try:
@@ -61,7 +66,7 @@ def deleteProduct(name):
                 if row['name'] != name:
                     rows.append(row)
         with open(filePath, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['name', 'quantity'])
+            writer = csv.DictWriter(file, fieldnames=['name', 'quantity', 'price'])
             writer.writeheader()
             writer.writerows(rows)
         print(f"Deleted product '{name}'.")
@@ -84,12 +89,15 @@ def main():
             readProducts()
         elif choice == '2':
             name = input("Enter product name: ")
-            quantity = input("Enter product quantity: ")
-            addProducts(name, quantity)
+            quantity = int(input("Enter product quantity: "))
+            price = int(input("Enter amount: Ksh."))
+            addProducts(name, quantity, price)
         elif choice == '3':
             name = input("Enter product name to update: ")
-            newQuantity = input("Enter new product quantity: ")
-            updateProduct(name, newQuantity)
+
+            newQuantity = int(input("Enter new product quantity: "))
+            price = int(input("Enter amount: Ksh."))
+            updateProduct(name, newQuantity, price)
         elif choice == '4':
             name = input("Enter product name to delete: ")
             deleteProduct(name)
