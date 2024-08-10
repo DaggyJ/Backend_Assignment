b1d730e70ff1c58b04467b6bc60e8af9df4f1718
DaggyJ <tranxporterdaggy@gmail.com>
2024-08-10 19:51:52 +0300

    main file created

--- Blogprj/Blogapp/admin.py
+++ Blogprj/Blogapp/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'description']
+    #list_filter = ['status']
+
+
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.site_header ='Todo App'
+admin.site.site_title ='BIBLE EXPOSITORIES'
--- Blogprj/Blogapp/urls.py
+++ Blogprj/Blogapp/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLonginView, RegisterPage, CustomLogoutView
+#from django.contrib.auth.views import LogoutView
+
+
 urlpatterns = [
      path('', TaskList.as_view(), name='task'),
     path('tasks-create/', TaskCreate.as_view(), name='tasks-create'),
+    path('login/', CustomLonginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'), 
+    path('register/', RegisterPage.as_view(), name='register'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     path('tasks-update/<int:pk>/', TaskUpdate.as_view(), name='tasks-update'),
     path('tasks-delete/<int:pk>/', TaskDelete.as_view(), name='tasks-delete'),
-
 
 ]
